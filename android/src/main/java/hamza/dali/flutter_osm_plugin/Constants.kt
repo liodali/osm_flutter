1c20ea57ee13d2c8a6bd8c3d9be3c8b042dc79b9
dali <lionelviperdali@gmail.com>
2020-12-05 14:34:29 +0100

    #11: start migration remove java class constantes and create new one in kotlin

---
+++ android/src/main/java/hamza/dali/flutter_osm_plugin/Constants.kt
@@ -0,0 +1,18 @@
+package hamza.dali.flutter_osm_plugin
+
+ class Constants {
+     
+    var unvailableAdress = "unvailable addresse"
+
+    enum class PositionMarker {
+        START, MIDDLE, END
+    }
+
+    companion object {
+        const val STARTPOSITIONROAD = "START"
+        const val MIDDLEPOSITIONROAD = "MIDDLE"
+        const val ENDPOSITIONROAD = "END"
+        const val nameFolderStatic = "staticPositionGeoPoint"
+
+    }
+}
