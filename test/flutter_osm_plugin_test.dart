c239d57ac330651bdb1a3aed7bcd4fd760b9933b
dali <lionelviperdali@gmail.com>
2020-12-18 18:41:09 +0100

    0.4.2: add test for calculation method

--- test/flutter_osm_plugin_test.dart
+++ test/flutter_osm_plugin_test.dart
@@ -1,2 +1,19 @@
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_osm_plugin/src/utilities.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() {}
+void main() {
+  test("calculation of distance between two points", () async {
+    double distance = await distance2point(
+      GeoPoint(
+        longitude: 36.84612143139903,
+        latitude: 11.099388684927824,
+      ),
+      GeoPoint(
+        longitude: 36.8388023164018,
+        latitude: 11.096959785428027,
+      ),
+    );
+    expect(distance.round(), 843);
+  });
+}
