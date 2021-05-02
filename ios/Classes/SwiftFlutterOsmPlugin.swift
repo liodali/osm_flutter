0d585c2a76e096b43c7134aa04a6022ce8b440d9
dali <lionelviperdali@gmail.com>
2021-05-02 19:34:42 +0100

    0.8.0.ios-alpha.0 fix call methodChannel

--- ios/Classes/SwiftFlutterOsmPlugin.swift
+++ ios/Classes/SwiftFlutterOsmPlugin.swift
@@ -1,21 +1,26 @@
 import Flutter
 import UIKit
 
+
 public class SwiftFlutterOsmPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let window = UIApplication.shared.delegate?.window
-    let controller = window??.rootViewController as! FlutterViewController
-   let mapViewFactory = MapviewFactory(controller: controller)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let window = UIApplication.shared.delegate?.window
+        let controller = window??.rootViewController as! FlutterViewController
 
-   //let channel = FlutterMethodChannel(name: "flutter_osm_plugin", binaryMessenger: registrar.messenger())
-   //let instance = SwiftFlutterOsmPlugin()
-   //registrar(forPlugin:"plugins.dali.hamza/osmview").register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
 
-   registrar.register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
+        //let channel = FlutterMethodChannel(name: "flutter_osm_plugin", binaryMessenger: registrar.messenger())
+        //let instance = SwiftFlutterOsmPlugin()
+        //registrar(forPlugin:"plugins.dali.hamza/osmview")
+        //.register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
 
-  // registrar.addMethodCallDelegate(instance, channel: channel)
- }
+        let mapViewFactory = MapviewFactory(controller: controller, messenger: controller.binaryMessenger)
 
- public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        registrar.register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
+
+        // registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+/* public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
    result("iOS " + UIDevice.current.systemVersion)
- }}
+ }*/
+}
