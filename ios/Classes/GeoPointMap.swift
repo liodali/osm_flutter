e40c39eef1613359d3177ba3ef266cf4bdacb080
dali <lionelviperdali@gmail.com>
2021-05-02 19:35:49 +0100

    0.7.5 add zoomDefault show marker in initPosition

--- ios/Classes/GeoPointMap.swift
+++ ios/Classes/GeoPointMap.swift
@@ -7,9 +7,9 @@
 import Foundation
 import MapKit
 
-class GeoPointMap: NSObject, MKAnnotation {
+class GeoPointMap:  NSObject,MKAnnotation {
   let title: String?
-  let discipline: String?
+  let subtitle: String?
   var marker:UIImage?
   let coordinate: CLLocationCoordinate2D
 
@@ -20,16 +20,34 @@ class GeoPointMap: NSObject, MKAnnotation {
     coordinate: CLLocationCoordinate2D
   ) {
     self.title = locationName
-    self.marker = icon
-    self.discipline = discipline
+    self.subtitle = discipline
     self.coordinate = coordinate
+    //super.init()
+    self.marker = icon
 
-    super.init()
   }
-    
+
     var location:CLLocation{
         return CLLocation(latitude: self.coordinate.latitude, longitude: self.coordinate.longitude)
     }
  
+}
+extension  GeoPointMap{
+  public func setupMKAnnotationView(
+          for annotation:GeoPointMap,on map:MKMapView
+  )-> MKAnnotationView{
+    let reuseIdentifier = NSStringFromClass(GeoPointMap.self)
+    let flagAnnotationView = map.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier, for: annotation)
+
+    flagAnnotationView.canShowCallout = false
+
+    // Provide the annotation view's image.
+    flagAnnotationView.image = self.marker
+
+
+
+
+    return flagAnnotationView
+  }
 }
 
