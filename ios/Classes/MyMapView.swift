d63e9a87f350c1db10b6691fbb9ed837f62205b7
dali <lionelviperdali@gmail.com>
2021-05-08 23:27:03 +0100

    #60 * add eventSink for StreamHandler * try to show osm tile * add CLLocationManager

--- ios/Classes/MyMapView.swift
+++ ios/Classes/MyMapView.swift
@@ -11,12 +11,18 @@ import UIKit
 import MapKit
 import Flutter
 
-public class MyMapView: NSObject, FlutterPlatformView,  MKMapViewDelegate {
+public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate,FlutterStreamHandler {
+
+
     let frame: CGRect
     let viewId: Int64
     let channel: FlutterMethodChannel
     let mapView: MKMapView
-    var markerIcon:UIImage? = nil
+    let locationManager : CLLocationManager  = CLLocationManager()
+    var markerIcon: UIImage? = nil
+    var isFollowUserLocation : Bool = false
+    var eventSink: FlutterEventSink? = nil
+    // var tileRenderer:MKTileOverlayRenderer!
 
     var span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
     var zoomDefault = 0.9
@@ -34,29 +40,33 @@ public class MyMapView: NSObject, FlutterPlatformView,  MKMapViewDelegate {
         mapview.isScrollEnabled = true
         self.mapView = mapview
         super.init()
+        self.mapView.delegate = self
 
-
+        self.setupTileRenderer()
         self.mapView.register(MKPinAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(GeoPointMap.self))
 
         channel.setMethodCallHandler({
             (call: FlutterMethodCall, result: FlutterResult) ->
-                    Void  in switch call.method {
+                    Void in
+            switch call.method {
             case "initPosition":
-                self.initPosition(args:call.arguments,result:result)
+                self.initPosition(args: call.arguments, result: result)
                 break;
             case "trackMe":
                 break;
             case "user#position":
                 break;
+            case "currentLocation ":
+                break;
             case "Zoom":
-               let levelZoom = call.arguments! as! Double
-                if(levelZoom == 0 || levelZoom == -1){
+                let levelZoom = call.arguments! as! Double
+                if (levelZoom == 0 || levelZoom == -1) {
                     var alpha = levelZoom
                     if levelZoom == 0 {
                         alpha = 1
                     }
-                    self.zoomMap(self.zoomDefault*alpha)
-                }else{
+                    self.zoomMap(self.zoomDefault * alpha)
+                } else {
                     self.zoomMap(levelZoom)
                 }
                 result(nil)
@@ -68,7 +78,7 @@ public class MyMapView: NSObject, FlutterPlatformView,  MKMapViewDelegate {
                 break;
             case "marker#icon":
                 print("marker")
-                self.markerIcon=self.convertImage(codeImage: call.arguments as! String)!
+                self.markerIcon = self.convertImage(codeImage: call.arguments as! String)!
                 result(200)
                 break;
             default:
@@ -79,33 +89,25 @@ public class MyMapView: NSObject, FlutterPlatformView,  MKMapViewDelegate {
 
         })
     }
+
     public func view() -> UIView {
-        if  #available(iOS 11.0,*) {
+        if #available(iOS 11.0, *) {
             self.mapView.register(
                     MarkerView.self,
                     forAnnotationViewWithReuseIdentifier:
                     MKMapViewDefaultAnnotationViewReuseIdentifier)
-            self.mapView.delegate = self
-        }
 
-        self.osmTile()
+        }
 
         return self.mapView
     }
-    private func osmTile(){
-        let urlTemplate = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        let overlay = MKTileOverlay(urlTemplate: urlTemplate)
-        overlay.canReplaceMapContent = true
-        self.mapView.addOverlay(overlay, level: .aboveLabels)
 
-
-    }
-    public func initPosition(args:Any?,  result:FlutterResult){
-        let pointInit = args as! Dictionary<String,Double>
+    private func initPosition(args: Any?, result: FlutterResult) {
+        let pointInit = args as! Dictionary<String, Double>
         print(pointInit)
-        let location = CLLocationCoordinate2D(latitude: pointInit["lat"]!, longitude: pointInit["lon"]!  )
+        let location = CLLocationCoordinate2D(latitude: pointInit["lat"]!, longitude: pointInit["lon"]!)
 
-        let geoPoint = GeoPointMap(locationName: "init location",icon: self.markerIcon , discipline: nil, coordinate: location)
+        let geoPoint = GeoPointMap(locationName: "init location", icon: self.markerIcon, discipline: nil, coordinate: location)
 
 //        let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
         let region = MKCoordinateRegion(center: location, span: self.span)
@@ -117,40 +119,78 @@ public class MyMapView: NSObject, FlutterPlatformView,  MKMapViewDelegate {
         result(200)
     }
 
+
     public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if(annotation is MKUserLocation){
+        if (annotation is MKUserLocation) {
             return nil
         }
-        var viewAnnotation:MKAnnotationView?
+        var viewAnnotation: MKAnnotationView?
         if let annotationM = annotation as? GeoPointMap {
             viewAnnotation = annotationM.setupMKAnnotationView(for: annotationM, on: mapView)
         }
 
         return viewAnnotation
     }
+   private func currentUserLocation() {
+       if let location = locationManager.location?.coordinate {
+           let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 4000, longitudinalMeters: 4000)
+           mapView.setRegion(region, animated: true)
+       }
+   }
 
-    private func convertImage(codeImage:  String) -> UIImage? {
+
+
+    private func convertImage(codeImage: String) -> UIImage? {
 
 
         let dataImage = Data(base64Encoded: codeImage)
         return UIImage(data: dataImage!)// Note it's optional. Don't force unwrap!!!
     }
 
-    private func zoomMap(_ level:Double){
+    private func zoomMap(_ level: Double) {
         var region = MKCoordinateRegion(center: self.mapView.region.center, span: self.mapView.region.span)
 
-        if(level>0){
+        if (level > 0) {
             region.span.latitudeDelta /= level
             region.span.longitudeDelta /= level
-        }else if(level<0){
+        } else if (level < 0) {
             let pLevel = abs(level)
-            region.span.latitudeDelta = min(region.span.latitudeDelta*pLevel,100.0)
-            region.span.longitudeDelta = min(region.span.longitudeDelta*pLevel,100.0)
+            region.span.latitudeDelta = min(region.span.latitudeDelta * pLevel, 100.0)
+            region.span.longitudeDelta = min(region.span.longitudeDelta * pLevel, 100.0)
         }
         self.mapView.setRegion(region, animated: true)
     }
 
+    private func setupTileRenderer() {
+
+        let template = "https://a.tile.openstreetmap.fr/osm/fr/{z}/{x}/{y}.png"
+
+        let overlay = MKTileOverlay(urlTemplate: template)
+        overlay.canReplaceMapContent = true
+        //self.mapView.addOverlay(overlay, level: .aboveLabels)
+        // self.tileRenderer = MKTileOverlayRenderer(tileOverlay: overlay)
+    }
+
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = events
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        self.eventSink = nil
+        return nil
+    }
+    public func mapView(
+            _ mapView: MKMapView,
+            rendererFor overlay: MKOverlay
+    ) -> MKOverlayRenderer {
+        guard let tileOverlay = overlay as? MKTileOverlay else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+        return MKTileOverlayRenderer(tileOverlay: tileOverlay)
+    }
 }
+
 private extension MKMapView {
     func centerToLocation(
             _ location: CLLocation,
@@ -161,7 +201,7 @@ private extension MKMapView {
                 latitudinalMeters: regionRadius,
                 longitudinalMeters: regionRadius
 
-                )
+        )
         setRegion(coordinateRegion, animated: true)
     }
 }
