40a7c24a58fccf4ac8cc28132d3e52f814ffe8c4
dali <lionelviperdali@gmail.com>
2021-05-09 13:53:50 +0100

    #60 * add currentLocation (change camera map to user location without draw marker or track location

--- ios/Classes/MyMapView.swift
+++ ios/Classes/MyMapView.swift
@@ -11,17 +11,17 @@ import UIKit
 import MapKit
 import Flutter
 
-public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate,FlutterStreamHandler {
+public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate, CLLocationManagerDelegate {
 
 
     let frame: CGRect
     let viewId: Int64
     let channel: FlutterMethodChannel
     let mapView: MKMapView
-    let locationManager : CLLocationManager  = CLLocationManager()
+    let locationManager: CLLocationManager = CLLocationManager()
     var markerIcon: UIImage? = nil
-    var isFollowUserLocation : Bool = false
-    var eventSink: FlutterEventSink? = nil
+    var isFollowUserLocation: Bool = false
+    var canGetLastUserLocation = false
     // var tileRenderer:MKTileOverlayRenderer!
 
     var span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
@@ -38,68 +38,77 @@ public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate,Flutter
         mapview.mapType = MKMapType.standard
         mapview.isZoomEnabled = true
         mapview.isScrollEnabled = true
-        self.mapView = mapview
+        mapView = mapview
         super.init()
-        self.mapView.delegate = self
 
-        self.setupTileRenderer()
-        self.mapView.register(MKPinAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(GeoPointMap.self))
+        /// affect delegation
+        mapView.delegate = self
+        locationManager.delegate = self;
+
+        //self.setupTileRenderer()
+        mapView.register(MKPinAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(GeoPointMap.self))
 
         channel.setMethodCallHandler({
             (call: FlutterMethodCall, result: FlutterResult) ->
                     Void in
-            switch call.method {
-            case "initPosition":
-                self.initPosition(args: call.arguments, result: result)
-                break;
-            case "trackMe":
-                break;
-            case "user#position":
-                break;
-            case "currentLocation ":
-                break;
-            case "Zoom":
-                let levelZoom = call.arguments! as! Double
-                if (levelZoom == 0 || levelZoom == -1) {
-                    var alpha = levelZoom
-                    if levelZoom == 0 {
-                        alpha = 1
-                    }
-                    self.zoomMap(self.zoomDefault * alpha)
-                } else {
-                    self.zoomMap(levelZoom)
-                }
-                result(nil)
-                break;
-            case "defaultZoom":
-                self.zoomDefault = call.arguments! as! Double
-
-                result(200)
-                break;
-            case "marker#icon":
-                print("marker")
-                self.markerIcon = self.convertImage(codeImage: call.arguments as! String)!
-                result(200)
-                break;
-            default:
-                result(nil)
-                break;
-            }
+            self.onListenMethodChannel(call:call,result:result)
 
 
         })
     }
-
+    private func onListenMethodChannel(call:FlutterMethodCall,result:FlutterResult){
+        print(call.method)
+        switch call.method {
+        case "initPosition":
+            initPosition(args: call.arguments, result: result)
+            break;
+        case "currentLocation":
+            currentUserLocation()
+            result(200)
+            break;
+        case "trackMe":
+            break;
+        case "user#position":
+            break;
+        case "deactivateTrackMe":
+            break;
+        case "Zoom":
+            let levelZoom = call.arguments! as! Double
+            if (levelZoom == 0 || levelZoom == -1) {
+                var alpha = levelZoom
+                if levelZoom == 0 {
+                    alpha = 1
+                }
+                zoomMap(zoomDefault * alpha)
+            } else {
+                zoomMap(levelZoom)
+            }
+            result(nil)
+            break;
+        case "defaultZoom":
+            zoomDefault = call.arguments! as! Double
+            result(200)
+            break;
+        case "marker#icon":
+            print("marker")
+            markerIcon = convertImage(codeImage: call.arguments as! String)!
+            result(200)
+            break;
+        default:
+            result(nil)
+            break;
+        }
+    }
     public func view() -> UIView {
         if #available(iOS 11.0, *) {
-            self.mapView.register(
+            mapView.register(
                     MarkerView.self,
                     forAnnotationViewWithReuseIdentifier:
                     MKMapViewDefaultAnnotationViewReuseIdentifier)
 
         }
 
-        return self.mapView
+        return mapView
     }
 
     private func initPosition(args: Any?, result: FlutterResult) {
@@ -107,18 +116,70 @@ public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate,Flutter
         print(pointInit)
         let location = CLLocationCoordinate2D(latitude: pointInit["lat"]!, longitude: pointInit["lon"]!)
 
-        let geoPoint = GeoPointMap(locationName: "init location", icon: self.markerIcon, discipline: nil, coordinate: location)
+        let geoPoint = GeoPointMap(locationName: "init location", icon: markerIcon, discipline: nil, coordinate: location)
 
 //        let span = MKCoordinateSpan(latitudeDelta: 2, longitudeDelta: 2)
-        let region = MKCoordinateRegion(center: location, span: self.span)
+        let region = MKCoordinateRegion(center: location, span: span)
 
-        self.mapView.setRegion(region, animated: true)
+        mapView.setRegion(region, animated: true)
 
-        self.mapView.addAnnotation(geoPoint)
+        mapView.addAnnotation(geoPoint)
         //self.mapView.centerToLocation(geoPoint.location)
         result(200)
     }
 
+
+
+    private func currentUserLocation() {
+        locationManager.requestLocation()
+         canGetLastUserLocation = true
+    }
+
+
+    private func convertImage(codeImage: String) -> UIImage? {
+        let dataImage = Data(base64Encoded: codeImage)
+        return UIImage(data: dataImage!)// Note it's optional. Don't force unwrap!!!
+    }
+
+    private func zoomMap(_ level: Double) {
+        var region = MKCoordinateRegion(center: mapView.region.center, span: mapView.region.span)
+
+        if (level > 0) {
+            region.span.latitudeDelta /= level
+            region.span.longitudeDelta /= level
+        } else if (level < 0) {
+            let pLevel = abs(level)
+            region.span.latitudeDelta = min(region.span.latitudeDelta * pLevel, 100.0)
+            region.span.longitudeDelta = min(region.span.longitudeDelta * pLevel, 100.0)
+        }
+        mapView.setRegion(region, animated: true)
+    }
+
+    private func setupTileRenderer() {
+
+        let template = "https://a.tile.openstreetmap.fr/osm/fr/{z}/{x}/{y}.png"
+
+        let overlay = MKTileOverlay(urlTemplate: template)
+        overlay.canReplaceMapContent = true
+        //self.mapView.addOverlay(overlay, level: .aboveLabels)
+        // self.tileRenderer = MKTileOverlayRenderer(tileOverlay: overlay)
+    }
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if(canGetLastUserLocation){
+            if let location = locations.last?.coordinate {
+                let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 4000, longitudinalMeters: 4000)
+                mapView.setRegion(region, animated: true)
+                //mapView.showsUserLocation = true
+                let geoMap = ["lon": location.longitude, "lat": location.latitude]
+                channel.invokeMethod("location", arguments: geoMap)
+                canGetLastUserLocation = false
+            }
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 
     public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if (annotation is MKUserLocation) {
@@ -131,55 +192,7 @@ public class MyMapView: NSObject, FlutterPlatformView, MKMapViewDelegate,Flutter
 
         return viewAnnotation
     }
-   private func currentUserLocation() {
-       if let location = locationManager.location?.coordinate {
-           let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 4000, longitudinalMeters: 4000)
-           mapView.setRegion(region, animated: true)
-       }
-   }
 
-
-
-    private func convertImage(codeImage: String) -> UIImage? {
-
-
-        let dataImage = Data(base64Encoded: codeImage)
-        return UIImage(data: dataImage!)// Note it's optional. Don't force unwrap!!!
-    }
-
-    private func zoomMap(_ level: Double) {
-        var region = MKCoordinateRegion(center: self.mapView.region.center, span: self.mapView.region.span)
-
-        if (level > 0) {
-            region.span.latitudeDelta /= level
-            region.span.longitudeDelta /= level
-        } else if (level < 0) {
-            let pLevel = abs(level)
-            region.span.latitudeDelta = min(region.span.latitudeDelta * pLevel, 100.0)
-            region.span.longitudeDelta = min(region.span.longitudeDelta * pLevel, 100.0)
-        }
-        self.mapView.setRegion(region, animated: true)
-    }
-
-    private func setupTileRenderer() {
-
-        let template = "https://a.tile.openstreetmap.fr/osm/fr/{z}/{x}/{y}.png"
-
-        let overlay = MKTileOverlay(urlTemplate: template)
-        overlay.canReplaceMapContent = true
-        //self.mapView.addOverlay(overlay, level: .aboveLabels)
-        // self.tileRenderer = MKTileOverlayRenderer(tileOverlay: overlay)
-    }
-
-    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        self.eventSink = events
-        return nil
-    }
-
-    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        self.eventSink = nil
-        return nil
-    }
     public func mapView(
             _ mapView: MKMapView,
             rendererFor overlay: MKOverlay
