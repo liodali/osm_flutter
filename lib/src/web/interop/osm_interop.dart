b1c51678ef58825771299159338a9e9f9446e4ba
dali <lionelviperdali@gmail.com>
2021-07-21 01:20:29 +0100

    #61 * add dart method`initMapLocation` to be called in js * add init map like mobile way * share method from window to iframe

--- lib/src/web/interop/osm_interop.dart
+++ lib/src/web/interop/osm_interop.dart
@@ -10,6 +10,15 @@ import 'models/geo_point_js.dart';
 @JS('locateMe')
 external Map<String, double> locateMe();
 
+@JS('initMapLocation')
+external dynamic initMapLocation(GeoPointJs p);
+
 @JS('addPosition')
 external dynamic addPosition(GeoPointJs p);
+
+/// Allows assigning a function to be callable from `window.initMapFinish()`
+@JS('initMapFinish')
+external set initMapFinish(void Function(bool) f);
+
+
 
