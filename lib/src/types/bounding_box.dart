35de1a96d1e72c9491f0c25cc1b0be983d6da157
dali <lionelviperdali@gmail.com>
2021-07-22 22:06:23 +0100

    feat: add new class `BoundingBox` (#129)

---
+++ lib/src/types/bounding_box.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+
+class BoundingBox {
+  final double north;
+  final double east;
+  final double south;
+  final double west;
+
+  BoundingBox({
+    required this.north,
+    required this.east,
+    required this.south,
+    required this.west,
+  })  : assert(north <= 85.0),
+        assert(east <= 180.0),
+        assert(south >= -85.0),
+        assert(west > -180.0);
+
+  const BoundingBox.world()
+      : this.north = 85.0,
+        this.east = 180.0,
+        this.south = -85.0,
+        this.west = -180.0;
+}
+
+extension ExtBoundingBox on BoundingBox {
+  bool isWorld() {
+    return north == 85.0 && east == 180.0 && south == -85.0 && west == -180.0;
+  }
+
+  bool inBoundingBox(GeoPoint point) {
+    bool latMatch = false;
+    bool lonMatch = false;
+    if (north < south) {
+      latMatch = true;
+    } else {
+      latMatch = (point.latitude < north) && (point.latitude > south);
+    }
+    if (east < west) {
+      lonMatch = point.longitude <= east && point.longitude >= west;
+    } else {
+      lonMatch = point.longitude < east && point.longitude > west;
+    }
+
+    return lonMatch && latMatch;
+  }
+}
