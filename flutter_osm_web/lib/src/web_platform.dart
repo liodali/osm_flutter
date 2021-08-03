9e0647376e7714ebd0b8592df3819995ecfefd1e
dali <lionelviperdali@gmail.com>
2021-08-03 21:24:19 +0100

    feat : add `onStaticGeoPointClicked` method to be call in js side (#61)

--- flutter_osm_web/lib/src/web_platform.dart
+++ flutter_osm_web/lib/src/web_platform.dart
@@ -1,35 +1,40 @@
 import 'package:flutter_osm_interface/flutter_osm_interface.dart';
+import 'package:flutter_osm_web/src/interop/models/geo_point_js.dart';
 import 'package:js/js.dart';
 
 import 'channel/method_channel_web.dart';
 import 'interop/osm_interop.dart' as interop;
-abstract class OsmWebPlatform extends OSMPlatform {
 
+abstract class OsmWebPlatform extends OSMPlatform {
   OsmWebPlatform() : super();
 
   //static FlutterOsmPluginWeb _instance = FlutterOsmPluginWeb(messenger: null);
 
   static int idOsmWeb = 0;
 
-  // static FlutterOsmPluginWeb get instance => _instance;
-  //
-  // /// Platform-specific plugins should set this with their own platform-specific
-  // /// class that extends [UrlLauncherPlatform] when they register themselves.
-  // static set instance(FlutterOsmPluginWeb instance) {
-  //   PlatformInterface.verifyToken(instance, BaseOsmPlatform.token);
-  //   _instance = instance;
-  // }
+// static FlutterOsmPluginWeb get instance => _instance;
+//
+// /// Platform-specific plugins should set this with their own platform-specific
+// /// class that extends [UrlLauncherPlatform] when they register themselves.
+// static set instance(FlutterOsmPluginWeb instance) {
+//   PlatformInterface.verifyToken(instance, BaseOsmPlatform.token);
+//   _instance = instance;
+// }
 }
-
-
-
 
 void BindingWebOSM() {
   interop.initMapFinish = allowInterop(initMapFinished);
+  interop.onStaticGeoPointClicked = allowInterop(onStaticGeoPointClicked);
 }
 
 void initMapFinished(bool isReady) {
-  final controller =
-      (OSMPlatform.instance as FlutterOsmPluginWeb).map!;//.mapsController[OsmWebPlatform.idOsmWeb]!;
+  final controller = (OSMPlatform.instance as FlutterOsmPluginWeb)
+      .map!; //.mapsController[OsmWebPlatform.idOsmWeb]!;
   controller.channel!.invokeMethod("initMap", isReady);
 }
+
+void onStaticGeoPointClicked(double lon, double lat) {
+  final controller = (OSMPlatform.instance as FlutterOsmPluginWeb).map!;
+  controller.channel!.invokeMethod("receiveGeoPoint", "$lat,$lon");
+}
+
