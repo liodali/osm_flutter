5e0b09035974199f3412f700c85527ba2d95226d
dali <lionelviperdali@gmail.com>
2021-11-07 17:17:00 +0100

    build : 0.25.0-rc.5 * update readme * format files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # flutter_osm_plugin
-![pub](https://img.shields.io/badge/pub-v0.25.0--rc.4-orange) 
+![pub](https://img.shields.io/badge/pub-v0.25.0--rc.5-orange) 
 
 ## Platform Support
 | Android | iOS | Web |
@@ -36,7 +36,7 @@
 Add the following to your `pubspec.yaml` file:
 
     dependencies:
-      flutter_osm_plugin: ^0.25.0-rc.3
+      flutter_osm_plugin: ^0.25.0-rc.5
 
 ### Migration to `0.16.0` (Android Only)
 > if you are using this plugin before Flutter 2 
--- example/lib/src/home/home_example.dart
+++ example/lib/src/home/home_example.dart
@@ -194,7 +194,8 @@ class _MainExampleState extends State<MainExample> with OSMMixinObserver {
           }),
           IconButton(
             onPressed: () async {
-              visibilityZoomNotifierActivation.value = !visibilityZoomNotifierActivation.value;
+              visibilityZoomNotifierActivation.value =
+                  !visibilityZoomNotifierActivation.value;
               zoomNotifierActivation.value = !zoomNotifierActivation.value;
             },
             icon: Icon(Icons.zoom_out_map),
@@ -226,7 +227,10 @@ class _MainExampleState extends State<MainExample> with OSMMixinObserver {
                   mainAxisSize: MainAxisSize.min,
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [CircularProgressIndicator(), Text("Map is Loading..")],
+                  children: [
+                    CircularProgressIndicator(),
+                    Text("Map is Loading..")
+                  ],
                 ),
               ),
               initZoom: 8,
@@ -261,7 +265,8 @@ class _MainExampleState extends State<MainExample> with OSMMixinObserver {
                       "${geoPoint.toMap().toString()}",
                     ),
                     action: SnackBarAction(
-                      onPressed: () => ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+                      onPressed: () =>
+                          ScaffoldMessenger.of(context).hideCurrentSnackBar(),
                       label: "hide",
                     ),
                   ),
@@ -345,7 +350,8 @@ class _MainExampleState extends State<MainExample> with OSMMixinObserver {
                   heroTag: "confirmAdvPicker",
                   onPressed: () async {
                     advPickerNotifierActivation.value = false;
-                    GeoPoint p = await controller.selectAdvancedPositionPicker();
+                    GeoPoint p =
+                        await controller.selectAdvancedPositionPicker();
                     print(p);
                   },
                 ),
@@ -530,7 +536,8 @@ class _MainExampleState extends State<MainExample> with OSMMixinObserver {
             showMarkerOfPOI: false,
           ),
         );
-        print("duration:${Duration(seconds: roadInformation.duration!.toInt()).inMinutes}");
+        print(
+            "duration:${Duration(seconds: roadInformation.duration!.toInt()).inMinutes}");
         print("distance:${roadInformation.distance}Km");
       });
     } on RoadException catch (e) {
