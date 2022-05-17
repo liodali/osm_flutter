707c49b0a80683be01aed510ced8a4bbc0be55f7
dali <lionelviperdali@gmail.com>
2022-05-17 20:56:28 +0200

    fix : change the type parameter of icon in `changeMarker` (#258) * replace the globalkey by MarkerIcon in method changeMarker

--- flutter_osm_interface/lib/src/osm_controller/osm_controller.dart
+++ flutter_osm_interface/lib/src/osm_controller/osm_controller.dart
@@ -29,7 +29,7 @@ abstract class IBaseOSMController {
   Future<void> changeMarker({
     required GeoPoint oldLocation,
     required GeoPoint newLocation,
-    GlobalKey? globalKeyIcon,
+    MarkerIcon? newMarkerIcon,
   });
 
   /// removeMarker
