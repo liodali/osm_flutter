4b41572e75c68eb34180e2e4e963b4afb7f4675a
liodali <lionelviperdali@gmail.com>
2023-06-18 18:15:16 +0200

    fix: fix rotate marker for android #420

--- android/src/main/kotlin/hamza/dali/flutter_osm_plugin/models/FlutterMaker.kt
+++ android/src/main/kotlin/hamza/dali/flutter_osm_plugin/models/FlutterMaker.kt
@@ -6,17 +6,22 @@ import android.graphics.Canvas
 import android.graphics.Matrix
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.BlendModeColorFilterCompat
 import androidx.core.graphics.BlendModeCompat
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import hamza.dali.flutter_osm_plugin.R
 import kotlinx.coroutines.CoroutineScope
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow
+import kotlin.math.PI
 
 typealias LongClickHandler = (marker: Marker) -> Boolean
 
@@ -24,6 +29,8 @@ open class FlutterMarker(private var mapView: MapView, var scope: CoroutineScope
     Marker(mapView),
     Marker.OnMarkerClickListener {
     private lateinit var context: Context
+    public var angle = 0.0
+        private set
     private var canvas: Canvas? = null
     var longPress: LongClickHandler? = null
         set(longPress) {
@@ -94,11 +101,52 @@ open class FlutterMarker(private var mapView: MapView, var scope: CoroutineScope
         return onClickListener?.onMarkerClick(this, mapView) ?: true
     }
 
-    fun setIconMaker(color: Int?, bitmap: Bitmap?, angle: Double = 0.0) {
-        getDefaultIconDrawable(color, bitmap, angle).also {
+    fun setIconMaker(color: Int? = null, bitmap: Bitmap?, angle: Double? = null) {
+        this.angle = angle ?: 0.0
+        getDefaultIconDrawable(color, bitmap, this.angle).also {
             icon = it
             setAnchor(0.5f, 0.5f)
         }
+    }
+
+    fun setIconMarkerFromURL(imageURL: String, angle: Double = 0.0) {
+        Picasso.get()
+            .load(imageURL)
+            .fetch(object : Callback {
+                override fun onSuccess() {
+                    Picasso.get()
+                        .load(imageURL)
+                        .into(object : Target {
+                            override fun onBitmapLoaded(
+                                bitmapMarker: Bitmap?,
+                                from: Picasso.LoadedFrom?
+                            ) {
+
+                                setIconMaker(bitmap = bitmapMarker, angle = angle)
+
+                            }
+
+                            override fun onBitmapFailed(
+                                e: java.lang.Exception?,
+                                errorDrawable: Drawable?
+                            ) {
+                                setIconMaker(bitmap = null, angle = angle)
+
+
+                            }
+
+                            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+                                // marker.icon = ContextCompat.getDrawable(context!!, R.drawable.ic_location_on_red_24dp)
+                            }
+
+                        })
+                }
+
+                override fun onError(e: java.lang.Exception?) {
+                    Log.e("error image", e?.stackTraceToString() ?: "")
+                }
+
+            })
     }
 
     fun defaultInfoWindow() {
@@ -119,10 +167,10 @@ open class FlutterMarker(private var mapView: MapView, var scope: CoroutineScope
         angle: Double = 0.0
     ): Drawable {
         var iconDrawable: Drawable? = null
-        bitmap?.let { b ->
+        bitmap?.let { bitmap ->
             iconDrawable = when (angle > 0.0) {
-                true -> BitmapDrawable(mapView.resources, rotateMarker(b, angle))
-                false -> BitmapDrawable(mapView.resources, b)
+                true -> BitmapDrawable(mapView.resources, rotateMarker(bitmap, angle))
+                false -> BitmapDrawable(mapView.resources, bitmap)
             }
             iconDrawable = iconDrawable.apply {
                 color?.let { c ->
@@ -143,7 +191,7 @@ open class FlutterMarker(private var mapView: MapView, var scope: CoroutineScope
 
     private fun rotateMarker(bitmap: Bitmap, angle: Double): Bitmap {
         val matrix = Matrix()
-        matrix.postRotate(angle.toFloat())
+        matrix.postRotate((angle * (180 / PI)).toFloat())
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     }
 
