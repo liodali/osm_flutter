c2e0757948650dee8a8b02a77a4de1ed35acf747
liodali <lionelviperdali@gmail.com>
2023-08-09 20:05:36 +0200

    fix hook example

--- example/lib/src/simple_example_hook.dart
+++ example/lib/src/simple_example_hook.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:osm_flutter_hooks/osm_flutter_hooks.dart';
 //import 'package:osm_flutter_hooks/osm_flutter_hooks.dart';
 
 class SimpleExample extends StatefulWidget {
@@ -35,15 +36,19 @@ class _SimpleExampleState extends State<SimpleExample> {
 class SimpleOSM extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    // final controller = useMapController(initMapWithUserPosition: true);
-    // useMapIsReady(
-    //   controller: controller,
-    //   mapIsReady: () async {
-    //     await controller.setZoom(zoomLevel: 15);
-    //   },
-    // );
+    final controller = useMapController(
+        initPosition: GeoPoint(
+      latitude: 47.4358055,
+      longitude: 8.4737324,
+    ));
+    useMapIsReady(
+      controller: controller,
+      mapIsReady: () async {
+        await controller.setZoom(zoomLevel: 15);
+      },
+    );
     return OSMFlutter(
-      controller: MapController(),
+      controller: controller,
       osmOption: OSMOption(
         markerOption: MarkerOption(
           defaultMarker: MarkerIcon(
