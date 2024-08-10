55455709c90416683020adbfad0e36c4241733b1
liodali <16631886+liodali@users.noreply.github.com>
2024-08-10 12:20:49 +0200

    update dart sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
-      initialRoute: "/simple",
+      initialRoute: "/home",
       routes: {
         "/home": (context) => const MainPageExample(),
         "/simple": (context) => Scaffold(
