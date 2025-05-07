37d164d5ad7b65909732119e30006b445d005483
liodali <16631886+liodali@users.noreply.github.com>
2025-05-07 18:01:43 +0200

    clean up our plugin.swift #588

--- ios/flutter_osm_plugin/Sources/flutter_osm_plugin/SwiftFlutterOsmPlugin.swift
+++ ios/flutter_osm_plugin/Sources/flutter_osm_plugin/SwiftFlutterOsmPlugin.swift
@@ -5,13 +5,7 @@ import UIKit
 public class SwiftFlutterOsmPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let window = UIApplication.shared.delegate?.window
-        let controller = window??.inputViewController //rootViewController as! FlutterViewController
 
-
-        //let channel = FlutterMethodChannel(name: "flutter_osm_plugin", binaryMessenger: registrar.messenger())
-        //let instance = SwiftFlutterOsmPlugin()
-        //registrar(forPlugin:"plugins.dali.hamza/osmview")
-        //.register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
         let keyDefaultPin = registrar.lookupKey(forAsset: "packages/flutter_osm_plugin/assets/default_pin.png")
         let mainBundle = Bundle.main
         let pathDefaultPin = mainBundle.path(forResource: keyDefaultPin, ofType: nil)
@@ -22,10 +16,6 @@ public class SwiftFlutterOsmPlugin: NSObject, FlutterPlugin {
 
         registrar.register(mapViewFactory, withId: "plugins.dali.hamza/osmview")
         
-        // registrar.addMethodCallDelegate(instance, channel: channel)
     }
 
-/* public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-   result("iOS " + UIDevice.current.systemVersion)
- }*/
 }
