fdd70333a34dd2fcd5f25c8806f7aa344a3b2d1b
liodali <16631886+liodali@users.noreply.github.com>
2025-07-16 09:39:49 +0200

    refactor: simplify code formatting and remove redundant async keywords
    - Remove redundant async/await in simple getter methods
    - Improve code readability by reducing line length

--- lib/src/controller/map_controller.dart
+++ lib/src/controller/map_controller.dart
@@ -3,8 +3,7 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_osm_interface/flutter_osm_interface.dart';
 import 'package:flutter_osm_plugin/src/controller/osm/osm_controller.dart';
-import 'package:permission_handler/permission_handler.dart'
-    show Permission, PermissionActions;
+import 'package:permission_handler/permission_handler.dart' show Permission, PermissionActions;
 
 /// class [MapController] : map controller that will control map by select position,enable current location,
 /// draw road , show static geoPoint,
@@ -232,7 +231,7 @@ class MapController extends BaseMapController {
   /// [getZoom]
   ///
   /// recuperate current zoom level
-  Future<double> getZoom() async => osmBaseController.getZoom();
+  Future<double> getZoom() => osmBaseController.getZoom();
 
   /// [setZoom]
   ///
@@ -508,8 +507,7 @@ class MapController extends BaseMapController {
     IconAnchor? iconAnchor,
   }) async {
     if (angle != null) {
-      assert(
-          angle >= 0 && angle <= 2 * pi, "angle should be between 0 and 2*pi");
+      assert(angle >= 0 && angle <= 2 * pi, "angle should be between 0 and 2*pi");
     }
     await osmBaseController.addMarker(
       p,
